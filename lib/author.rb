8ea5f7dc7f9062aac4b69ca4b692d807c4703d12
Nicolas Bermudez <bermnicolas@gmail.com>
2019-02-01 21:55:32 +0000

    Automatically backed up by Learn

---
+++ lib/author.rb
@@ -0,0 +1,27 @@
+class Author 
+  attr_accessor :name
+
+  def initialize(name)
+    @name=name
+    @posts = []
+  end
+
+  def posts 
+    @posts 
+  end
+
+  def add_post(post)
+    @posts << posts
+    post.author=self
+  end
+
+  def add_post_by_title(name)
+    post = Post.new(name)
+    @posts << post
+    post.author = self
+  end
+
+  def self.song_count
+    
+  end
+end
