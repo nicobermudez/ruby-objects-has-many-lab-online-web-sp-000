12dc6156bd4425ff0260c06852c1ec0fb6353ec4
Nicolas Bermudez <bermnicolas@gmail.com>
2019-02-01 21:42:30 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -17,7 +17,11 @@ class Artist
 
   def add_song_by_name(name)
     song = Song.new(name)
-    @songs << song
+    @songs << song 
     song.artist = self
+  end
+
+  def self.song_count
+    self.songs.count
   end
 end
