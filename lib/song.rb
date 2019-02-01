5c6f40ab9bdd3ce700d10ae0d90c2d718012a5ad
Nicolas Bermudez <bermnicolas@gmail.com>
2019-02-01 21:38:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,15 +12,11 @@ class Song
   end
 
   def name
-    @name
+    @name 
   end
 
-  def artist
-    self.artist 
-  end
-
-  def artist_name
-    self.artist.name
+  def artist_name 
+    self.artist.name 
   end
 
 
